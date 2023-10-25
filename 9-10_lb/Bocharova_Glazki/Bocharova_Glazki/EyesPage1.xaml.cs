867393a06afefd3623f22358db03b3adc285c36b
j0pka <jo.pka@icloud.com>
2023-10-25 17:56:20 +0300

    9 и 10 лаба + скрипт БД

---
+++ 9-10_lb/Bocharova_Glazki/Bocharova_Glazki/EyesPage1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bocharova_Glazki
+{
+    /// <summary>
+    /// Логика взаимодействия для EyesPage1.xaml
+    /// </summary>
+    public partial class EyesPage1 : Page
+    {
+        public EyesPage1()
+        {
+            InitializeComponent();
+            var currentService = Bocharova_GlazkiEntities.GetContext().Product.ToList();
+            EyesListView.ItemsSource = currentService;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+       
+    }
+}
